e1d4af146111ea126d0580d9569a505d2dd034e4
Milan Gazda <gazda@mgazda.cz>
2019-10-14 13:08:12 +0200

    Uprava pro composer

---
+++ src/StringUtils.php
@@ -0,0 +1,24 @@
+<?php
+
+class StringUtils {
+
+    /**
+     * Metoda hleda bud text nebo pole textu pomoci metody strpos.
+     */
+    public static function strpos($haystack, $needle) {
+        if (!is_array($needle)) {
+            $needle = [$needle];
+        }
+
+        if (!empty($needle)) {
+            foreach ($needle as $findMe) {
+                if (strpos($haystack, $findMe) !== false) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
