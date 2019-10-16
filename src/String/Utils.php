f11afdab77bf7f2b3efcaa0b1d46b3a0032b9239
Milan Gazda <gazda@mgazda.cz>
2019-10-16 14:18:35 +0200

    Doprogramovani UpperCamelCase metody do String Utils

--- src/String/Utils.php
+++ src/String/Utils.php
@@ -2,6 +2,8 @@
 
 namespace MGazdaCz\PhpLibraries\String;
 
+use Nette\Utils\Strings;
+
 class Utils {
 
     /**
@@ -21,6 +23,27 @@ class Utils {
         }
 
         return false;
+    }
+    
+    public static function upperCamelCase(string $string) {
+      $string = Strings::toAscii($string);
+      $string = Strings::lower($string);
+      
+      $stringArray = explode(' ', $string);
+      
+      if (!empty($stringArray)) {
+        foreach ($stringArray as $key => $s) {
+          if (strlen($s) > 0) {
+            $s{0} = strtoupper($s{0});
+            
+            $stringArray[$key] = $s;
+          }
+        }
+        
+        $string = implode('', $stringArray);
+      }
+      
+      return $string;
     }
 
 }
